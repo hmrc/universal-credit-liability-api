e33065c6329c663eaa0dcf2fddc85ae49b8dc090
Su-Yang Yu <suyangyu@users.noreply.github.com>
2025-06-11 13:56:47 +0100

    WIP: adding skeleton endpoint (#7)
    * adding skeleton endpoint
    
    * added some workarounds
    
    ---------
    
    Co-authored-by: Su-Yang Yu <20665693+suyangyu@users.noreply.github.com>

---
+++ it/test/uk/gov/hmrc/universalcreditliabilityapi/NotificationIntegrationSpec.scala
@@ -0,0 +1,99 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.universalcreditliabilityapi
+
+import com.github.tomakehurst.wiremock.client.WireMock.*
+import com.github.tomakehurst.wiremock.stubbing.StubMapping
+import play.api.http.Status
+import play.api.libs.json.Json
+import play.api.libs.ws.{WSClient, WSResponse, writeableOf_JsValue}
+import uk.gov.hmrc.universalcreditliabilityapi.support.WireMockIntegrationSpec
+
+import java.util.UUID
+
+class NotificationIntegrationSpec extends WireMockIntegrationSpec {
+
+  private val wsClient = app.injector.instanceOf[WSClient]
+  private val baseUrl  = s"http://localhost:$port"
+
+  "POST /notification" must {
+    "return 204 when HIP returns 204 for insert" in {
+      val nino = ":nino"
+
+      stubHipInsert(nino)
+
+      val response = callPostNotification(false)
+
+      response.status mustBe Status.NO_CONTENT
+
+      verify(1, postRequestedFor(urlEqualTo(s"/person/$nino/liability/universal-credit")))
+    }
+
+    "return 204 when HIP returns 204 for terminate" in {
+      val nino = ":nino"
+
+      stubHipTermination(nino)
+
+      val response = callPostNotification(true)
+
+      response.status mustBe Status.NO_CONTENT
+
+      verify(1, postRequestedFor(urlEqualTo(s"/person/$nino/liability/universal-credit/termination")))
+    }
+  }
+
+  def stubHipInsert(nino: String): StubMapping =
+    stubFor(
+      post(urlEqualTo(s"/person/$nino/liability/universal-credit"))
+        .willReturn(
+          aResponse()
+            .withHeader("content-type", "application/json")
+            .withHeader("correlationId", TestData.correlationId)
+            .withHeader("gov-uk-originator-id", TestData.testOriginatorId)
+            .withBody(Json.parse("{}").toString())
+            .withStatus(204)
+        )
+    )
+
+  def stubHipTermination(nino: String): StubMapping =
+    stubFor(
+      post(urlEqualTo(s"/person/$nino/liability/universal-credit/termination"))
+        .willReturn(
+          aResponse()
+            .withHeader("correlationId", TestData.correlationId)
+            .withHeader("gov-uk-originator-id", TestData.testOriginatorId)
+            .withStatus(204)
+        )
+    )
+
+  def callPostNotification(terminate: Boolean): WSResponse =
+    wsClient
+      .url(s"$baseUrl/notification")
+      .withHttpHeaders(
+        "correlationId"        -> TestData.correlationId,
+        "gov-uk-originator-id" -> TestData.testOriginatorId,
+        // TODO remove temp workaround
+        "terminate"            -> terminate.toString
+      )
+      .post(Json.parse("{}"))
+      .futureValue
+}
+
+object TestData {
+  val testOriginatorId      = ""
+  val correlationId: String = UUID.randomUUID().toString
+}
